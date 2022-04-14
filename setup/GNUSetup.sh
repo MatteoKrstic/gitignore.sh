8e6c6b7d18541f521ad226cc36b2a9c59498fb0b
Thebigbot <77632836+Thebigbot0000@users.noreply.github.com>
2022-04-14 19:55:57 +0200

    GNU setup

--- setup/GNUSetup.sh
+++ setup/GNUSetup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+echo -e "\e[93mAdd gsh in your system...\e[0m"
 touch ~/gsh.sh
 CON="$(curl https://raw.githubusercontent.com/MatteoKrstic/gitignore.sh/main/main.sh)"
 echo "$CON" >> ~/gsh.sh
 echo "alias gsh='sh ~/gsh.sh'" >> ~/.bashrc
+alias gsh='sh ~/gsh.sh'
+echo -e "\e[92mAdd gsh in your system...\e[0m"
