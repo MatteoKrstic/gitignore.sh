198b89c64d5fd5e9faf7033a1b543215cbab1599
Thebigbot <77632836+Thebigbot0000@users.noreply.github.com>
2022-04-14 19:22:39 +0200

    Linux installer (no animation)

--- setup/GNUSetup.sh
+++ setup/GNUSetup.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+
+touch ~/gsh.sh
+CON="$(curl https://raw.githubusercontent.com/MatteoKrstic/gitignore.sh/main/main.sh)"
+echo "$CON" >> ~/gsh.sh
+echo "alias gsh='sh ~/gsh.sh'" >> ~/.bash_aliases
