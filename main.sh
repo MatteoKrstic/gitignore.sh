70e7fb52bbdbcc96b4ba83045c4d6ee84f9f9a17
grabbino <krsticmatteo@gmail.com>
2022-04-14 17:14:28 +0200

    🧊 main file with base comments

---
+++ main.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+# This script is used to run the main program.
+# The scope of the program is to be able to generate every gitignore troughout a terminal command.
+
+# made by Valerio C.(Thebigbot#2222) and Matteo K.(Matteo Krstic#8397)
+# version 1.0
