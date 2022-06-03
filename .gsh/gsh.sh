e8578c4428346fa85d0e0df544738f119d1f4130
MatteoKrsticDev <krsticmatteo@gmail.com>
2022-06-03 16:28:09 +0200

    starting all over again

--- .gsh/gsh.sh
+++
@@ -1,1 +0,0 @@
-# This file content is the same as main.sh
--- main.ps1
+++
@@ -1,9 +0,0 @@
-# This script is used to run the main program.
-# The scope of the program is to be able to generate every gitignore troughout a terminal command.
-# The .ps1 files are made only for windows.
-
-# made by Valerio C.(Thebigbot#2222) and Matteo K.(Matteo Krstic#8397)
-# version 1.0
-
-Write-Output "hello from gsh"
-
